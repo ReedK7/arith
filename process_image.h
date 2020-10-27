5f3912b7a18cbb191f3e4ff95ff3280bc2d3849f
Nick Murphy <71105164+nicksmurf7@users.noreply.github.com>
2020-10-26 21:00:11 -0400

    Create process_image.h

---
+++ process_image.h
@@ -0,0 +1,19 @@
+
+#include "arith40.h"
+#include "a2methods.h"
+#include "uarray.h"
+#include "assert.h"
+#include "except.h"
+#include "bitpack.h"
+#include "uarray2b.h"
+#include "a2blocked.h"
+#include "pnm.h"
+#include "pnmrdr.h"
+#include "uarray2.h"
+
+
+void shave_column(int col, int row, UArray2b_T array2b, void *elem,
+void *cl);
+void shave_row(int col, int row, UArray2b_T array2b, void *elem,
+void *cl);
+Pnm_ppm prep(Pnm_ppm pic);
